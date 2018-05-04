85a3e1ba32b23e6455365829aa6fb9eb93b9da4f
Johannes Egger <j.egger@posteo.at>
2018-05-04 21:31:13 +0200

    Bind dev server to 0.0.0.0

--- src/Client/webpack.config.js
+++ src/Client/webpack.config.js
@@ -35,6 +35,8 @@ module.exports = {
     modules: [ resolve("../../node_modules/")]
   },
   devServer: {
+    host: '0.0.0.0',
+    port: 8080,
     proxy: {
       '/api/*': {
         target: 'http://localhost:' + port,
