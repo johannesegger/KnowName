fc23fdf54d96bc111659ea992c150d54dbd17163
Johannes Egger <j.egger@posteo.at>
2018-10-20 19:45:03 +0200

    Move images in ignored folder

--- set-env.ps1
+++ set-env.ps1
@@ -1,4 +1,4 @@
 $env:DOTNET_ROOT=Split-Path -Parent (Get-Command dotnet).Path
 $env:SISDB_CONNECTION_STRING="Server=localhost;Port=8081;Database=sis2;User=root;Password=1234"
-$env:TEACHER_IMAGE_DIR="$PSScriptRoot\images\teachers"
-$env:STUDENT_IMAGE_DIR="$PSScriptRoot\images\students"
+$env:TEACHER_IMAGE_DIR="$PSScriptRoot\test\images\teachers"
+$env:STUDENT_IMAGE_DIR="$PSScriptRoot\test\images\students"
